66ddcd4d971ffa84f82a96a8effd1a014e38a2af
Aman Deep Kujur <amandeepkujur56@gmail.com>
2021-09-29 23:37:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './Styles.css'
+import Header from './Components/Header'
+// import Left from './Components/Left'
+import Right from './Components/Right'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            
+        }
+    }
+
+    render(){
+
+        return (
+            <div>
+                {/* header */}
+                    <Header/>
+                {/* end of header */}
+                <div className="content">
+                    {/* left content */}
+                        {/* <Left/> */}
+                    {/* right content i.e notes */}
+                        <Right/>
+                </div>
+                {/* end of content */}
+            </div>
+        )
+
+    }
 }
 
-export default App;
+export default App
