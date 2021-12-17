05ba8416c510401888cb5b3811d8dd9dd34699cf
Aman Deep Kujur <amandeepkujur56@gmail.com>
2021-12-17 16:10:07 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-//       target: 'https://notekeeper--backend.herokuapp.com',
-      target: 'https://app-notekeeper-backend.netlify.app',
+      target: 'https://notekeeper--backend.herokuapp.com',
+//       target: 'https://app-notekeeper-backend.netlify.app',
       changeOrigin: true,
     })
   );
